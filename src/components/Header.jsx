e77b0102122e700a232fc55b6e3ecb771d2aadd9
RafaelTecnicoDoLar <rjonasmombellifranco@gmail.com>
2024-11-08 10:55:10 -0300

    atualizacao almoco

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <header>
+            <h1 className="Titulo_Pedido">
+                Quer namorar comigo Mariane Hanae Yokota?
+
+            </h1>
+
+        </header>
+
+    );
+}
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
+import Header from './components/Header'
+import Button from './components/Button'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Header />
+    <Button />
+
   </StrictMode>,
 )
